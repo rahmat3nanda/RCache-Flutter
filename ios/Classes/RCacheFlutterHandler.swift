626eeddaccd084627444fd83633a1c9f6f96fb92
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-24 21:28:47 +0700

    refactor ios module

--- ios/Classes/RCacheFlutterHandler.swift
+++ ios/Classes/RCacheFlutterHandler.swift
@@ -6,10 +6,16 @@
 //
 
 import Flutter
+import RCache
 
 internal class RCacheFlutterHandler {
     internal class func common(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         
+        if (call.method == RCacheFlutterMethod.Key.clear) {
+            RCache.common.clear()
+            result("Success clear")
+            return
+        }
     }
     
     internal class func credentials(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
