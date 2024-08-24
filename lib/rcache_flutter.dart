7d7d5fa3fae6f5f2d6a774faa2676d95c6fec702
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-24 19:59:44 +0700

    remove unused

--- lib/rcache_flutter.dart
+++
@@ -1,8 +0,0 @@
-
-import 'rcache_flutter_platform_interface.dart';
-
-class RcacheFlutter {
-  Future<String?> getPlatformVersion() {
-    return RcacheFlutterPlatform.instance.getPlatformVersion();
-  }
-}
