fdbc55408c83ef34c772dd5b906fdd23e2c962f1
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-24 20:01:28 +0700

    create core

---
+++ lib/common_rcache_method_channel.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:rcache_flutter/rcache_helper.dart';
+import 'package:rcache_flutter/rcache_key.dart';
+import 'package:rcache_flutter/rcache_method.dart';
+import 'package:rcache_flutter/rcache_platform_interface.dart';
+import 'package:rcache_flutter/rcaching.dart';
+
+/// An implementation of [RCachePlatform] that uses method channels.
+class CommonMethodChannelRCache extends RCachePlatform implements RCaching {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = MethodChannel(RCacheMethod.channel);
+
+  @override
+  Future<void> saveData({
+    required Uint8List data,
+    required RCacheKey key,
+  }) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.data,
+        rArgs(type: RCacheMethod.key.common, key: key, value: data),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveString({
+    required String string,
+    required RCacheKey key,
+  }) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.string,
+        rArgs(type: RCacheMethod.key.common, key: key, value: string),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveBool({required bool value, required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.bool,
+        rArgs(type: RCacheMethod.key.common, key: key, value: value),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveInteger({required int value, required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.integer,
+        rArgs(type: RCacheMethod.key.common, key: key, value: value),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveArray({
+    required List<dynamic> array,
+    required RCacheKey key,
+  }) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.array,
+        rArgs(type: RCacheMethod.key.common, key: key, value: array),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveDictionary({
+    required Map<String, dynamic> dictionary,
+    required RCacheKey key,
+  }) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.dictionary,
+        rArgs(type: RCacheMethod.key.common, key: key, value: dictionary),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> saveDouble({
+    required double value,
+    required RCacheKey key,
+  }) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.save.double,
+        rArgs(type: RCacheMethod.key.common, key: key, value: value),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<Uint8List?> readData({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.read.data,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<String?> readString({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.read.string,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<bool?> readBool({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.read.bool,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<int?> readInteger({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.read.integer,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<List<dynamic>?> readArray({required RCacheKey key}) async {
+    try {
+      List<dynamic>? value = await methodChannel.invokeMethod(
+        RCacheMethod.key.read.array,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+      return value == null ? null : rAutoCastList(value);
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>?> readDictionary({required RCacheKey key}) async {
+    try {
+      Map<Object?, Object?>? value = await methodChannel.invokeMethod(
+        RCacheMethod.key.read.dictionary,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+      return Future.value(
+        value == null ? null : rAutoCastMap(value.cast<String, dynamic>()),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<double?> readDouble({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.read.double,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> remove({required RCacheKey key}) async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.remove,
+        rArgs(type: RCacheMethod.key.common, key: key),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> clear() async {
+    try {
+      return await methodChannel.invokeMethod(
+        RCacheMethod.key.clear,
+        rArgs(type: RCacheMethod.key.common),
+      );
+    } on PlatformException catch (e) {
+      return Future.error(e);
+    }
+  }
+}
---
+++ lib/rcache_platform_interface.dart
@@ -0,0 +1,78 @@
+import 'dart:typed_data';
+
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:rcache_flutter/common_rcache_method_channel.dart';
+import 'package:rcache_flutter/credentials_rcache_method_channel.dart';
+import 'package:rcache_flutter/rcache_key.dart';
+
+abstract class RCachePlatform extends PlatformInterface {
+  /// Constructs a RCachePlatform.
+  RCachePlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static RCachePlatform _common = CommonMethodChannelRCache();
+
+  /// The default instance of [RCachePlatform] to use.
+  ///
+  /// Defaults to [MethodChannelRCache].
+  static RCachePlatform get common => _common;
+
+  /// Platform-specific implementations should set this with their own
+  /// platform-specific class that extends [RCachePlatform] when
+  /// they register themselves.
+  static set common(RCachePlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _common = instance;
+  }
+
+  static RCachePlatform _credentials = CredentialsMethodChannelRCache();
+
+  /// The default instance of [RCachePlatform] to use.
+  ///
+  /// Defaults to [MethodChannelRCache].
+  static RCachePlatform get credentials => _credentials;
+
+  /// Platform-specific implementations should set this with their own
+  /// platform-specific class that extends [RCachePlatform] when
+  /// they register themselves.
+  static set credentials(RCachePlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _credentials = instance;
+  }
+
+  Future<void> saveData({required Uint8List data, required RCacheKey key});
+
+  Future<void> saveString({required String string, required RCacheKey key});
+
+  Future<void> saveBool({required bool value, required RCacheKey key});
+
+  Future<void> saveInteger({required int value, required RCacheKey key});
+
+  Future<void> saveArray({required List<dynamic> array, required RCacheKey key});
+
+  Future<void> saveDictionary({
+    required Map<String, dynamic> dictionary,
+    required RCacheKey key,
+  });
+
+  Future<void> saveDouble({required double value, required RCacheKey key});
+
+  Future<Uint8List?> readData({required RCacheKey key});
+
+  Future<String?> readString({required RCacheKey key});
+
+  Future<bool?> readBool({required RCacheKey key});
+
+  Future<int?> readInteger({required RCacheKey key});
+
+  Future<List<dynamic>?> readArray({required RCacheKey key});
+
+  Future<Map<String, dynamic>?> readDictionary({required RCacheKey key});
+
+  Future<double?> readDouble({required RCacheKey key});
+
+  Future<void> remove({required RCacheKey key});
+
+  Future<void> clear();
+}
