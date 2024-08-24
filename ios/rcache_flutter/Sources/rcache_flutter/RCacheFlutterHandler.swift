f1e099c377d7b19cbc30829125d153da3864087e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-24 20:34:34 +0700

    Create RCacheFlutterHandler.swift

---
+++ ios/rcache_flutter/Sources/rcache_flutter/RCacheFlutterHandler.swift
@@ -0,0 +1,22 @@
+//
+//  RCacheFlutterHandler.swift
+//  rcache
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 24/08/24.
+//
+
+import Flutter
+
+internal class RCacheFlutterHandler {
+    internal class func common(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+    }
+    
+    internal class func credentials(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+    }
+    
+    internal class func clear(result: @escaping FlutterResult) {
+        
+    }
+}
