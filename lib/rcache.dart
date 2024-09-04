36b8a0f13948bd9748317c3cc5387c08ef880c78
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 20:28:23 +0700

    update impl

--- lib/rcache.dart
+++ lib/rcache.dart
@@ -3,9 +3,23 @@ import 'package:rcache_flutter/rcaching.dart';
 
 export 'package:rcache_flutter/rcache_key.dart';
 
+/// This is a main class for managing cache with RCache
 class RCache {
-  static RCaching common = RCachePlatform.common as RCaching;
-  static RCaching credentials = RCachePlatform.credentials as RCaching;
+  /// Get a RCaching instance with common level. Used to store RCache for regular data.
+  ///
+  /// @return RCaching
+  ///
+  /// @note This instance is an instance of CommonMethodChannelRCache.
+  static RCaching common = RCachePlatform.common;
+
+  /// Get a RCaching instance with credentials level. Used to store RCache for credentials data.
+  ///
+  /// @return RCaching
+  ///
+  /// @note This instance is an instance of KeychainRCache.
+  static RCaching credentials = RCachePlatform.credentials;
+
+  /// Method to delete all data for common and credential levels stored via RCache/RCaching.
 
   static void clear() async {
     common.clear();
