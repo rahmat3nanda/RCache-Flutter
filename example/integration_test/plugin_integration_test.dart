5b45f8cf446866a56afd7c786066a3cb191eb7f9
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 21:13:37 +0700

    update implementation

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -16,38 +16,38 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Common test', (WidgetTester tester) async {
-    await RCache.common.saveData(
-      data: _values.data,
-      key: _RCacheKeyTest.data,
+    await RCache.common.saveUint8List(
+      _values.uint8list,
+      key: _RCacheKeyTest.uint8list,
     );
     await RCache.common.saveString(
-      string: _values.string,
+      _values.string,
       key: _RCacheKeyTest.string,
     );
     await RCache.common.saveBool(
-      value: _values.boolValue,
+      _values.boolValue,
       key: _RCacheKeyTest.bool,
     );
     await RCache.common.saveInteger(
-      value: _values.intValue,
+      _values.intValue,
       key: _RCacheKeyTest.int,
     );
     await RCache.common.saveArray(
-      array: _values.array,
+      _values.array,
       key: _RCacheKeyTest.array,
     );
-    await RCache.common.saveDictionary(
-      dictionary: _values.dictionary,
-      key: _RCacheKeyTest.dictionary,
+    await RCache.common.saveMap(
+      _values.map,
+      key: _RCacheKeyTest.map,
     );
     await RCache.common.saveDouble(
-      value: _values.doubleValue,
+      _values.doubleValue,
       key: _RCacheKeyTest.double,
     );
 
     expect(
-      await RCache.common.readData(key: _RCacheKeyTest.data),
-      _values.data,
+      await RCache.common.readUint8List(key: _RCacheKeyTest.uint8list),
+      _values.uint8list,
     );
     expect(
       await RCache.common.readString(key: _RCacheKeyTest.string),
@@ -66,8 +66,8 @@ void main() {
       _values.array,
     );
     expect(
-      await RCache.common.readDictionary(key: _RCacheKeyTest.dictionary),
-      _values.dictionary,
+      await RCache.common.readMap(key: _RCacheKeyTest.map),
+      _values.map,
     );
     expect(
       await RCache.common.readDouble(key: _RCacheKeyTest.double),
@@ -76,38 +76,38 @@ void main() {
   });
 
   testWidgets('Credentials test', (WidgetTester tester) async {
-    await RCache.credentials.saveData(
-      data: _values.data,
-      key: _RCacheKeyTest.data,
+    await RCache.credentials.saveUint8List(
+      _values.uint8list,
+      key: _RCacheKeyTest.uint8list,
     );
     await RCache.credentials.saveString(
-      string: _values.string,
+      _values.string,
       key: _RCacheKeyTest.string,
     );
     await RCache.credentials.saveBool(
-      value: _values.boolValue,
+      _values.boolValue,
       key: _RCacheKeyTest.bool,
     );
     await RCache.credentials.saveInteger(
-      value: _values.intValue,
+      _values.intValue,
       key: _RCacheKeyTest.int,
     );
     await RCache.credentials.saveArray(
-      array: _values.array,
+      _values.array,
       key: _RCacheKeyTest.array,
     );
-    await RCache.credentials.saveDictionary(
-      dictionary: _values.dictionary,
-      key: _RCacheKeyTest.dictionary,
+    await RCache.credentials.saveMap(
+      _values.map,
+      key: _RCacheKeyTest.map,
     );
     await RCache.credentials.saveDouble(
-      value: _values.doubleValue,
+      _values.doubleValue,
       key: _RCacheKeyTest.double,
     );
 
     expect(
-      await RCache.credentials.readData(key: _RCacheKeyTest.data),
-      _values.data,
+      await RCache.credentials.readUint8List(key: _RCacheKeyTest.uint8list),
+      _values.uint8list,
     );
     expect(
       await RCache.credentials.readString(key: _RCacheKeyTest.string),
@@ -126,8 +126,8 @@ void main() {
       _values.array,
     );
     expect(
-      await RCache.credentials.readDictionary(key: _RCacheKeyTest.dictionary),
-      _values.dictionary,
+      await RCache.credentials.readMap(key: _RCacheKeyTest.map),
+      _values.map,
     );
     expect(
       await RCache.credentials.readDouble(key: _RCacheKeyTest.double),
@@ -137,7 +137,7 @@ void main() {
 }
 
 class _RCacheKeyTest {
-  static RCacheKey get data => RCacheKey("data");
+  static RCacheKey get uint8list => RCacheKey("uint8list");
 
   static RCacheKey get string => RCacheKey("string");
 
@@ -147,37 +147,37 @@ class _RCacheKeyTest {
 
   static RCacheKey get array => RCacheKey("array");
 
-  static RCacheKey get dictionary => RCacheKey("dictionary");
+  static RCacheKey get map => RCacheKey("map");
 
   static RCacheKey get double => RCacheKey("double");
 }
 
 class _RCacheValueModelTest {
-  final Uint8List data;
+  final Uint8List uint8list;
   final String string;
   final bool boolValue;
   final int intValue;
   final List array;
-  final Map<String, dynamic> dictionary;
+  final Map<String, dynamic> map;
   final double doubleValue;
 
   _RCacheValueModelTest({
-    required this.data,
+    required this.uint8list,
     required this.string,
     required this.boolValue,
     required this.intValue,
     required this.array,
-    required this.dictionary,
+    required this.map,
     required this.doubleValue,
   });
 }
 
 _RCacheValueModelTest _values = _RCacheValueModelTest(
-  data: Uint8List.fromList("RCache".codeUnits),
+  uint8list: Uint8List.fromList("RCache".codeUnits),
   string: "RCache",
   boolValue: false,
   intValue: 1,
   array: [1, "b", true],
-  dictionary: {"a": 1, "b": "c"},
+  map: {"a": 1, "b": "c"},
   doubleValue: 12.123,
 );
