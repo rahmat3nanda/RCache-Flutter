4076c20bf940a87a9e1e7b6570cb22f75699fade
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 20:50:54 +0700

    update usage

--- test/rcache_method_channel_test.dart
+++ test/rcache_method_channel_test.dart
@@ -8,12 +8,12 @@ import 'rcache_key_test.dart';
 import 'rcache_value_model_test.dart';
 
 RCacheValueModelTest _values = RCacheValueModelTest(
-  data: Uint8List.fromList("RCache".codeUnits),
+  uint8List: Uint8List.fromList("RCache".codeUnits),
   string: "RCache",
   boolValue: false,
   intValue: 1,
   array: [1, "b", true],
-  dictionary: {"a": 1, "b": "c"},
+  map: {"a": 1, "b": "c"},
   doubleValue: 12.123,
 );
 
@@ -31,8 +31,8 @@ void main() {
       (MethodCall methodCall) async {
         String method = methodCall.method;
 
-        if (method == RCacheMethod.key.read.data) {
-          return _values.data;
+        if (method == RCacheMethod.key.read.uint8List) {
+          return _values.uint8List;
         }
 
         if (method == RCacheMethod.key.read.string) {
@@ -51,8 +51,8 @@ void main() {
           return _values.array;
         }
 
-        if (method == RCacheMethod.key.read.dictionary) {
-          return _values.dictionary;
+        if (method == RCacheMethod.key.read.map) {
+          return _values.map;
         }
 
         if (method == RCacheMethod.key.read.double) {
@@ -70,15 +70,15 @@ void main() {
   });
 
   test('Common Type', () async {
-    expect(await common.readData(key: RCacheKeyTest.data), _values.data);
+    expect(
+      await common.readUint8List(key: RCacheKeyTest.uint8List),
+      _values.uint8List,
+    );
     expect(await common.readString(key: RCacheKeyTest.string), _values.string);
     expect(await common.readBool(key: RCacheKeyTest.bool), _values.boolValue);
     expect(await common.readInteger(key: RCacheKeyTest.int), _values.intValue);
     expect(await common.readArray(key: RCacheKeyTest.array), _values.array);
-    expect(
-      await common.readDictionary(key: RCacheKeyTest.dictionary),
-      _values.dictionary,
-    );
+    expect(await common.readMap(key: RCacheKeyTest.map), _values.map);
     expect(
       await common.readDouble(key: RCacheKeyTest.double),
       _values.doubleValue,
@@ -86,19 +86,25 @@ void main() {
   });
 
   test('Credentials Type', () async {
-    expect(await credentials.readData(key: RCacheKeyTest.data), _values.data);
-    expect(await credentials.readString(key: RCacheKeyTest.string),
-        _values.string);
+    expect(
+      await credentials.readUint8List(key: RCacheKeyTest.uint8List),
+      _values.uint8List,
+    );
+    expect(
+      await credentials.readString(key: RCacheKeyTest.string),
+      _values.string,
+    );
     expect(
         await credentials.readBool(key: RCacheKeyTest.bool), _values.boolValue);
-    expect(await credentials.readInteger(key: RCacheKeyTest.int),
-        _values.intValue);
     expect(
-        await credentials.readArray(key: RCacheKeyTest.array), _values.array);
-    expect(
-      await credentials.readDictionary(key: RCacheKeyTest.dictionary),
-      _values.dictionary,
+      await credentials.readInteger(key: RCacheKeyTest.int),
+      _values.intValue,
     );
+    expect(
+      await credentials.readArray(key: RCacheKeyTest.array),
+      _values.array,
+    );
+    expect(await credentials.readMap(key: RCacheKeyTest.map), _values.map);
     expect(
       await credentials.readDouble(key: RCacheKeyTest.double),
       _values.doubleValue,
