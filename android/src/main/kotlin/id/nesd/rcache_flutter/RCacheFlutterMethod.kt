2d33c35761bcd4df62f392dd72abebe73fc1e87f
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 20:56:16 +0700

    update key

--- android/src/main/kotlin/id/nesd/rcache_flutter/RCacheFlutterMethod.kt
+++ android/src/main/kotlin/id/nesd/rcache_flutter/RCacheFlutterMethod.kt
@@ -17,24 +17,24 @@ internal class RCacheFlutterMethod {
 
         class Save {
             companion object {
-                const val data: String = "saveData"
+                const val uint8List: String = "saveUint8List"
                 const val string: String = "saveString"
                 const val bool: String = "saveBool"
                 const val integer: String = "saveInteger"
                 const val array: String = "saveArray"
-                const val dictionary: String = "saveDictionary"
+                const val map: String = "saveMap"
                 const val double: String = "saveDouble"
             }
         }
 
         class Read {
             companion object {
-                const val data: String = "readData"
+                const val uint8List: String = "readUint8List"
                 const val string: String = "readString"
                 const val bool: String = "readBool"
                 const val integer: String = "readInteger"
                 const val array: String = "readArray"
-                const val dictionary: String = "readDictionary"
+                const val map: String = "readMap"
                 const val double: String = "readDouble"
             }
         }
