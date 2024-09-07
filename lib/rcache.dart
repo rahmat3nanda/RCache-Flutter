86063ab3e21dd87fadf829b1c607d6dbf18cc70e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-07 15:09:33 +0700

    Fix

--- lib/rcache.dart
+++ lib/rcache.dart
@@ -20,7 +20,7 @@ class RCache {
   static RCaching credentials = RCachePlatform.credentials;
 
   /// Method to delete all data for common and credential levels stored via RCache/RCaching.
-  static void clear() async {
+  static Future<void> clear() async {
     common.clear();
     credentials.clear();
   }
